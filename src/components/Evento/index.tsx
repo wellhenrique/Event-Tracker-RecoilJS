54303a0711950b7b6834032992d9b5b0eca0babb
wellhenrique <welissonh80@gmail.com>
2023-06-02 12:00:53 -0300

    refactor: excluindo e alterando o evento com useSetRecoilState em vez de passar um callback por props

--- src/components/Evento/index.tsx
+++ src/components/Evento/index.tsx
@@ -2,9 +2,25 @@ import React from 'react';
 import { IEvento } from '../../interfaces/IEvento'
 import style from './Evento.module.scss';
 import EventoCheckbox from './EventoCheckbox';
+import { useSetRecoilState } from 'recoil';
+import { listaDeEventosState } from '../../states/atom';
 
-const Evento: React.FC<{ evento: IEvento, aoAlterarStatus: (id: number) => void, aoDeletarEvento: (id: number) => void }> = ({ evento, aoAlterarStatus, aoDeletarEvento }) => {
-  
+const Evento: React.FC<{ evento: IEvento }> = ({ evento }) => {
+
+  const setListaDeEventos = useSetRecoilState<IEvento[]>(listaDeEventosState)
+
+  const excluirEvento = () => {
+    setListaDeEventos(listaAntiga => listaAntiga.filter(eventos => eventos.id !== evento.id))
+  }
+
+  const alterarStatusEvento = () => {
+    evento.completo = !evento.completo
+    setListaDeEventos(listaAntiga => {
+      const indice = listaAntiga.findIndex(eventos => eventos.id === evento.id)
+      return [...listaAntiga.slice(0, indice), evento, ...listaAntiga.slice(indice + 1)]
+    })
+  }
+
   const estilos = [
     style.Evento
   ]
@@ -15,11 +31,11 @@ const Evento: React.FC<{ evento: IEvento, aoAlterarStatus: (id: number) => void,
 
   return (<div className={estilos.join(' ')}>
 
-    <EventoCheckbox evento={evento} aoAlterarStatus={aoAlterarStatus}/>
+    <EventoCheckbox evento={evento} aoAlterarStatus={alterarStatusEvento} />
     <div className="cards-info">
       <h3 className={style.descricao}>{evento.descricao} - {evento.inicio.toLocaleDateString()}</h3>
     </div>
-    <i className="far fa-times-circle fa-2x" onClick={() => aoDeletarEvento(evento.id!)}></i>
+    <i className="far fa-times-circle fa-2x" onClick={excluirEvento}></i>
   </div>)
 }
 
