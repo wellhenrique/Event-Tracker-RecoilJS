db5ba9200430b1cc86e4d496beffcd7fe237c699
wellhenrique <welissonh80@gmail.com>
2023-06-02 11:59:18 -0300

    refactor: listando os eventos usando o useSetRecoilState com listaDeEventosState

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,27 +1,36 @@
 import React, { useState } from 'react';
 import { IEvento } from '../../interfaces/IEvento';
 import style from './Formulario.module.scss';
+import { obterId } from '../../utils/ids';
+import { useSetRecoilState } from 'recoil';
+import { listaDeEventosState } from '../../states/atom';
 
-const Formulario: React.FC<{ aoSalvar: (evento: IEvento) => void }> = ({ aoSalvar }) => {
+const Formulario: React.FC = () => {
   const [descricao, setDescricao] = useState('')
   const [dataInicio, setDataInicio] = useState('')
   const [horaInicio, setHoraInicio] = useState('')
   const [dataFim, setDataFim] = useState('')
   const [horaFim, setHoraFim] = useState('')
 
-  const montarData = (data:string, hora: string) => {
+  const setListaDeEventos = useSetRecoilState<IEvento[]>(listaDeEventosState)
+
+  const montarData = (data: string, hora: string) => {
     const dataString = data.slice(0, 10)
     return new Date(`${dataString}T${hora}`)
   }
 
   const submeterForm = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
-    aoSalvar({
+    const evento = {
+      id: obterId(),
       descricao,
       inicio: montarData(dataInicio, horaInicio),
       fim: montarData(dataFim, horaFim),
       completo: false
-    })
+    }
+
+    setListaDeEventos(listaAntiga => [...listaAntiga, evento])
+
     setDescricao('')
     setDataInicio('')
     setHoraInicio('')
@@ -32,51 +41,51 @@ const Formulario: React.FC<{ aoSalvar: (evento: IEvento) => void }> = ({ aoSalva
     <h3 className={style.titulo}>Novo evento</h3>
 
     <label>Descrição</label>
-    <input 
-      type="text" 
+    <input
+      type="text"
       name="descricao"
       id="descricao"
       className={style.input}
-      onChange={evento => setDescricao(evento.target.value)} 
-      placeholder="Descrição" value={descricao} 
+      onChange={evento => setDescricao(evento.target.value)}
+      placeholder="Descrição" value={descricao}
       autoComplete="off"
       required />
 
-      <label>Data de início</label>
-      <div className={style.inputContainer}>
-        <input 
-          type="date" 
-          name="dataInicio"
-          className={style.input}
-          onChange={evento => setDataInicio(evento.target.value)} 
-          value={dataInicio}
-          required />
-        <input 
-          type="time" 
-          name="horaInicio"
-          className={style.input}
-          onChange={evento => setHoraInicio(evento.target.value)} 
-          value={horaInicio}
-          required />
-      </div>
+    <label>Data de início</label>
+    <div className={style.inputContainer}>
+      <input
+        type="date"
+        name="dataInicio"
+        className={style.input}
+        onChange={evento => setDataInicio(evento.target.value)}
+        value={dataInicio}
+        required />
+      <input
+        type="time"
+        name="horaInicio"
+        className={style.input}
+        onChange={evento => setHoraInicio(evento.target.value)}
+        value={horaInicio}
+        required />
+    </div>
 
-      <label>Data de término</label>
-      <div className={style.inputContainer}>
-        <input 
-          type="date" 
-          name="dataFim"
-          className={style.input}
-          onChange={evento => setDataFim(evento.target.value)} 
-          value={dataFim}
-          required />
-        <input 
-          type="time" 
-          name="horaFim"
-          className={style.input}
-          onChange={evento => setHoraFim(evento.target.value)} 
-          value={horaFim}
-          required />
-      </div>
+    <label>Data de término</label>
+    <div className={style.inputContainer}>
+      <input
+        type="date"
+        name="dataFim"
+        className={style.input}
+        onChange={evento => setDataFim(evento.target.value)}
+        value={dataFim}
+        required />
+      <input
+        type="time"
+        name="horaFim"
+        className={style.input}
+        onChange={evento => setHoraFim(evento.target.value)}
+        value={horaFim}
+        required />
+    </div>
 
     <button className={style.botao}>
       Salvar
